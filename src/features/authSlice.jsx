21f68fe93e8584c13d3faa527643da9ed8f294f6
BruceHill <brucehillwalley@gmail.com>
2024-01-08 02:54:40 +0300

    authSlice

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,11 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const initialState={
+  user:"",
+  loading:"false",
+  error:"false",
+  token:"",
+}
+
 const authSlice = createSlice({
   name: "auth",
 
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    fetchStart:(state)=>{
+      state.loading=true
+
+    },
+    loginSucces:(state, {payload})=>{
+      state.loading=false
+      state.user=payload.user.username
+      state.token=payload.token
+
+    },
+    fetchFail:(state)=>{
+        state.loading=false
+        state.error=true
+
+    }
+  },
 })
 
-export const {} = authSlice.actions
+export const {fetchFail,fetchStart,loginSucces} = authSlice.actions
 export default authSlice.reducer
