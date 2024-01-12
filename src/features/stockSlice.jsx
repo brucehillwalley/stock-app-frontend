a409439f4fdb2f117887204a116f79989bb5e189
BruceHill <brucehillwalley@gmail.com>
2024-01-12 04:15:20 +0300

    feat: add stockSlice starter

---
+++ src/features/stockSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+firms:[],
+brands:[],
+products:[],
+purchases:[],
+sales:[],
+
+}
+
+const stockSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = stockSlice.actions
+
+export default stockSlice.reducer
