e097b35b33ef6eecf19f60b760f6e4e7a31f9a4b
BruceHill <brucehillwalley@gmail.com>
2024-01-15 22:27:47 +0300

    conditional rendering

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,33 +6,27 @@ import { useSelector } from "react-redux"
 import { Grid } from "@mui/material"
 import ProductModal from "../components/ProductModal"
 import ProductTable from "../components/ProductTable"
+import { ErrorMsg, NoDataMsg } from "../components/DataFetchMsg"
 
 const Products = () => {
   // const { getFirms, getSales } = useStockCalls()
   const { getStocks } = useStockCalls()
-  const { products } = useSelector((state) => state.stock)
+  const { products, error, loading } = useSelector((state) => state.stock)
 
-  const initialState = {
-    categoryId: "",
-    brandId: "",
-    name: "",
-  
-  }
-  const [info, setInfo] = useState({
-    initialState
-  })
+  const initialState = { categoryId: "", brandId: "", name: "" }
+  const [info, setInfo] = useState(initialState)
 
   const [open, setOpen] = useState(false)
   const handleOpen = () => setOpen(true)
   const handleClose = () => {
     setOpen(false)
-    setInfo({ name: "", phone: "", address: "", image: "" })
+    setInfo(initialState)
   }
 
   useEffect(() => {
     getStocks("products")
-    getStocks("categories")
-    getStocks("brands")
+    // getStocks("categories")
+    // getStocks("brands")
   }, [])
 
   return (
@@ -51,7 +45,11 @@ const Products = () => {
         setInfo={setInfo}
       />
 
-      <ProductTable />
+      {error && <ErrorMsg />}
+
+      {!error && !products.lenght && <NoDataMsg />}
+
+      {!error && <ProductTable />}
     </div>
   )
 }
