083a8d32d1ba518711f16bec3b0899308e7c0fe6
BruceHill <brucehillwalley@gmail.com>
2024-01-16 20:17:36 +0300

    feat:modalStyle

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -2,8 +2,8 @@ import { Button, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
 import useStockCalls from "../service/useStockCalls";
 import { useSelector } from "react-redux";
-import FirmCardCard from "../components/FirmCard";
 import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/FirmModal";
 
 const Firms = () => {
   // const { getFirms, getSales } = useStockCalls();
@@ -21,6 +21,9 @@ const Firms = () => {
         Firms
       </Typography>
       <Button variant="contained">New Firm</Button>
+
+      <FirmModal/>
+
       <Grid container gap={2} mt={3} justifyContent={"center"}>
      {firms?.map((firm)=>(
 
